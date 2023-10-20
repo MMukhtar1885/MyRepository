8f7d9e5572c2602b1cf0a607f8facf4fc3ef0028
MMukhtar1885 <146448557+MMukhtar1885@users.noreply.github.com>
2023-10-20 15:45:19 +0100

    update

--- 2 Tardy Trains/Program.cs
+++ 2 Tardy Trains/Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.WriteLine("Tardy Train Monitoring Application");
 Console.WriteLine("How many minutes was the train late?");
 
+int MinutesLate = int.Parse(Console.ReadLine());
+
+while (MinutesLate > 0)
+{
+    Console.WriteLine("How many minutes was the train late?");
+}
