78241961b53a47c67d9665d2d525b85d5cd14a5f
mohammedmukhtar05 <M.O.MUKHTAR-2022@hull.ac.uk>
2023-10-22 15:19:26 +0100

    unfinished

--- 2 Tardy Trains/Program.cs
+++ 2 Tardy Trains/Program.cs
@@ -3,14 +3,7 @@ Console.WriteLine("How many minutes was the train late?");
 
 int MinutesLate = int.Parse(Console.ReadLine());
 
-while (MinutesLate > 0)
+do
 {
-    Console.WriteLine("How many minutes was the train late?");
-}
-
-
-// work on it at home
-
-//i was able to access it
-
-// 1885
+    Console.WriteLine("How many minutes was the train late?"); 
+} while (MinutesLate > 0);
