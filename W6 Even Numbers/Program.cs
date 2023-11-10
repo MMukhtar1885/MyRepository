b1b38d9a8477fc443a8b9914d8fffd38d647985b
MMukhtar1885 <146448557+MMukhtar1885@users.noreply.github.com>
2023-11-10 14:56:19 +0000

    sh4trbgfgx

---
+++ W6 Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a list of numbers seperated by spaces.");
+Console.WriteLine("The program will output all the even numbers.");
+
+string input = Console.ReadLine();
+
+string output = string.Empty;
+
+string[] separateStrings = input.Split(' ');
+
+foreach (string str in separateStrings)
+{
+    string seperateStrings = str;   
+
+}
+
+int number = int.Parse(output); 
+
+Console.WriteLine(number);  
+
+if (number % 2 == 0)
+{
+    Console.Write(number + " ");
+}
