949e4b70f25165604ff148cc18e62d537c124b7b
MMukhtar1885 <146448557+MMukhtar1885@users.noreply.github.com>
2023-11-10 15:33:24 +0000

    fd tszbcfrdze

---
+++ W6 Even More Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter a list of numbers seperated by spaces.");
+Console.WriteLine("The program will output all the odd numbers, then output all the even numbers.");
+ 
+string input = Console.ReadLine();
+ 
+string output = string.Empty;
+
+string[] separatestring = input.Split(" ");
+
+int number = int.Parse(output);
+
+string str = string.Empty;
+
+foreach (string separate in separatestring)
+{
+    number = int.Parse(str);
+    if (number % 3 == 0)
+    {
+        Console.Write(number + " ");
+    }
+}
--- W6 Even Numbers/Program.cs
+++ W6 Even Numbers/Program.cs
@@ -1,24 +1,30 @@
 ﻿Console.WriteLine("Hello, World!");
+
 Console.WriteLine("Enter a list of numbers seperated by spaces.");
+
 Console.WriteLine("The program will output all the even numbers.");
 
 string input = Console.ReadLine();
 
-string output = string.Empty;
+string output = input;
+
+int number = int.Parse(output);
 
 string[] separateStrings = input.Split(' ');
 
 foreach (string str in separateStrings)
 {
-    string seperateStrings = str;   
 
+    separateStrings = str;
+    if (number % 2 == 0)
+    {
+        Console.Write(number + " ");
+    }
+
+    Console.WriteLine(number);
 }
 
-int number = int.Parse(output); 
 
-Console.WriteLine(number);  
 
-if (number % 2 == 0)
-{
-    Console.Write(number + " ");
-}
+
+
