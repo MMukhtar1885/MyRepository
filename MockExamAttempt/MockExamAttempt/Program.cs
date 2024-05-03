cc9e0f19144b0d12e1b73673d7cda8383d896bd5
MMukhtar1885 <146448557+MMukhtar1885@users.noreply.github.com>
2024-05-03 02:07:46 +0100

    created new projects for different questions

--- MockExamAttempt/MockExamAttempt/Program.cs
+++ MockExamAttempt/MockExamAttempt/Program.cs
@@ -1,126 +1,4 @@
-﻿int TipTapTop = 0;
-
-
-for (int i = 1; i <= 100; i++)
-{
-    if (i % 2 == 0 && i % 6 == 0)
-    {
-        if (sumofstring(i.ToString()) % 2 != 0)
-        {
-            TipTapTop++;
-        }
-    }
-
-}
-Console.WriteLine($"TipTapTop is {TipTapTop}"); 
-
-    int sumofstring(string tosString)
-{
-    int sumofstring = 0;
-        foreach (char c in tosString)
-        {
-            sumofstring += c - '0';
-        }
-        return sumofstring;
-}
-
-
-
-
-
-
-
-//TickTockSock question
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int tickCount = 0;
-//            int tockCount = 0;
-//            int tickTockCount = 0;
-//            int shockCount = 0;
-
-//            for (int num = 1; num <= 100; num++)
-//            {
-//                switch (num)
-//                {
-//                    case var _ when num % 3 == 0 && num % 5 == 0:
-//                        Console.WriteLine("TickTock");
-//                        tickTockCount++;
-//                        break;
-//                    case var _ when num % 3 == 0:
-//                        Console.WriteLine("Tick");
-//                        tickCount++;
-//                        break;
-//                    case var _ when num % 5 == 0:
-//                        Console.WriteLine("Tock");
-//                        tockCount++;
-//                        break;
-//                    case var _ when num % 7 == 0 && num % 3 != 0 && num % 5 != 0:
-//                        Console.WriteLine("SHOCK");
-//                        shockCount++;
-//                        break;
-//                    default:
-//                        Console.WriteLine(num);
-//                        break;
-//                }
-//            }
-
-//            Console.WriteLine("\nSummary:");
-//            Console.WriteLine($"Total numbers displayed: 100");
-//            Console.WriteLine($"Tick count: {tickCount}");
-//            Console.WriteLine($"Tock count: {tockCount}");
-//            Console.WriteLine($"TickTock count: {tickTockCount}");
-//            Console.WriteLine($"SHOCK count: {shockCount}");
-//        }
-//    }
-//}
-
-//static void Main()
-//{
-//    int tickCount = 0;
-//    int tockCount = 0;
-//    int ticktockCount = 0;
-//    int shockCount = 0;
-//    int numberCount = 0;
-
-//        for (int num = 1; num <= 100; num++)
-//        {
-
-
-//            if (num % 3 == 0)
-//            {
-//                tickCount++;
-//            }
-
-//            else if (num % 5 == 0)
-//            {
-//                tockCount++;
-//            }
-//            else if (num % 3 == 0 && num % 5 == 0)
-//            {
-//                ticktockCount++;
-//            }
-
-//            else if (num % 7 == 0 && num % 3 != 0 && num % 5 != 0)
-//            {
-//                shockCount++;
-//            }
-
-
-//            else //(num % 3 != 0 && num % 5 != 0 && num % 7 != 0)
-//            {
-//                numberCount++;
-//            }
-//        }
-
-//        Console.WriteLine($"Tick= {tickCount}");
-//        Console.WriteLine($"Tock= {tockCount}");
-//        Console.WriteLine($"TickTock= {ticktockCount}");
-//        Console.WriteLine($"SHOCK= {shockCount}");
-//        Console.WriteLine($"numbers= {numberCount}");
-//    }
-//}
+﻿//using System.Runtime.Intrinsics.X86;
 
 //Write a program that automatically plays the TickTock SHOCK game from 1 to 10,000 (inclusive of the values 1 and 10,000)
 
@@ -143,7 +21,7 @@ Console.WriteLine($"TipTapTop is {TipTapTop}");
 
 //Numbers divisible by neither three, five nor seven should just output the number
 
-
+ 
 
 //For example, a typical round of TickTock SHOCK would start as follows:
 
@@ -156,12 +34,55 @@ Console.WriteLine($"TipTapTop is {TipTapTop}");
 //SHOCK is displayed 2 times(in place of 7, 14)
 //Etc......Use this example to test your code from 1 - 100 (inclusive of the values 1 and 100) before changing it to run 1-10000:
 
-//Input Expected Output
-//Game from 1 - 100 (inclusive)	Tick    27
-//Tock	14
-//TickTock	6
-//SHOCK	8
-//numbers	45
+
+// TH BELOW ANSWER IS CORRECT
 
 
-//For the game of TickTock SHOCK (numbers 1 - 10,000 inclusive):
+static void Main()
+{
+    int tickCount = 0;
+    int tockCount = 0;
+    int ticktockCount = 0;
+    int shockCount = 0;
+    int numberCount = 0;
+
+    for (int num = 1; num <= 100; num++)
+    {
+
+        if (num % 3 == 0 && num % 5 == 0)
+        {
+            ticktockCount++;
+        }
+
+        else if (num % 3 == 0)
+        {
+            tickCount++;
+        }
+
+        else if (num % 5 == 0)
+        {
+            tockCount++;
+        }
+
+
+        else if (num % 7 == 0 && num % 3 != 0 && num % 5 != 0)
+        {
+            shockCount++;
+        }
+
+
+        else //(num % 3 != 0 && num % 5 != 0 && num % 7 != 0)
+        {
+            numberCount++;
+        }
+    }
+
+    Console.WriteLine($"Tick= {tickCount}");
+    Console.WriteLine($"Tock= {tockCount}");
+    Console.WriteLine($"TickTock= {ticktockCount}");
+    Console.WriteLine($"SHOCK= {shockCount}");
+    Console.WriteLine($"numbers= {numberCount}");
+}
+
+
+
---
+++ MockExamAttempt/Question 2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Below is some code in a basic console application that should create an array of size 20 and assign all the elements of the array that have an odd index to the value 1
+
+//There is an error in the code.
+
+
+int[] array = new int[20];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0) //the issue was swtiching >= to !=, its now correct
+    {
+        array[i] = 1;
+    }
+}
+
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+Console.WriteLine(array[5]);
+Console.WriteLine(array[6]);
+Console.WriteLine(array[7]);
+Console.WriteLine(array[8]);
