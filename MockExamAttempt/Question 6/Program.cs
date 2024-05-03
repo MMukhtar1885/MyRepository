ff771d73aeed6bc566b0f94b97c89a39c7290ea5
MMukhtar1885 <146448557+MMukhtar1885@users.noreply.github.com>
2024-05-03 14:53:03 +0100

    Added Q6

---
+++ MockExamAttempt/Question 6/Program.cs
@@ -0,0 +1,20 @@
+﻿string solutionPath = @"C:\Users\796433.ADIR\Source\Repos\MMukhtar1885\Lab-Work\MockExamAttempt\Question 6\dogOwners.txt";
+
+string[] fileContent = File.ReadAllLines(solutionPath);
+
+char[] delimiterChars = { '[', ']', ':', ','};
+
+
+for (int i = 0; i < fileContent.Length - 1; i++)
+{
+
+ string[] words = fileContent[i].Split(delimiterChars);
+
+    for (int j = 3; j <= words.Length - 1 ; j = j+2 )
+     {
+        Console.WriteLine(words[j]);
+    }
+
+
+}
+
