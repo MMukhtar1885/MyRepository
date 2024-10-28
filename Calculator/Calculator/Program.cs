238f082eb282959afee36b165af60518f013c946
MOHAMMED MUKHTAR <M.O.MUKHTAR-2022@hull.ac.uk>
2024-10-28 15:21:17 +0000

    added the calculator project to this repo

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Calculations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            return (a / b);
+        }
+    }
+}
