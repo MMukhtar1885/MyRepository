36083651634c4fec7e901e40e426630d42d0fbcf
mohammedmukhtar05 <M.O.MUKHTAR-2022@hull.ac.uk>
2024-10-30 23:25:53 +0000

    finished task 3 but still not desired outcome on terminal

--- DBConnect/DBConnect/Program.cs
+++ DBConnect/DBConnect/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// continue tomorrow
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+
+string connStr ="server=localhost;user=root;database=world;port=3306;password=Mukhtar2580";
+MySqlConnection conn = new MySqlConnection(connStr);
+
+ static void Main()
+ {
+string connStr ="server=localhost;user=root;database=world;port=3306;password=Mukhtar2580";
+ MySqlConnection conn = new MySqlConnection(connStr);
+ try
+ {
+ Console.WriteLine("Connecting to MySQL--- world database");
+ conn.Open();
+ // Perform database operations
+ string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+ MySqlCommand cmd = new MySqlCommand(sql, conn);
+ MySqlDataReader rdr = cmd.ExecuteReader();
+ while (rdr.Read())
+ {
+ Console.WriteLine(rdr[0]+" -- "+rdr[1]);
+ }
+ rdr.Close();
+ }
+ catch (Exception ex)
+ {
+ Console.WriteLine(ex.ToString());
+ }
+ conn.Close(); // close the connection
+ Console.WriteLine("Done the job");
+ }
+
