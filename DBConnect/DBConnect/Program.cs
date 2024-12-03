76c428e9bc6a934e88fe7374411c78f43cfe78c6
MOHAMMED MUKHTAR <M.O.MUKHTAR-2022@hull.ac.uk>
2024-12-03 11:53:13 +0000

    fixed it

--- DBConnect/DBConnect/Program.cs
+++ DBConnect/DBConnect/Program.cs
@@ -1,32 +1,25 @@
 ﻿using MySql.Data;
 using MySql.Data.MySqlClient;
 
-string connStr ="server=localhost;user=root;database=world_connection;port=3306;password=Mukhtar2580";
-MySqlConnection conn = new MySqlConnection(connStr);
 
- static void Main()
- {
-string connStr ="server=localhost;user=root;database=world_connection;port=3306;password=Mukhtar2580";
- MySqlConnection conn = new MySqlConnection(connStr);
- try
- {
- Console.WriteLine("Connecting to MySQL--- world database");
- conn.Open();
- // Perform database operations
- string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
- MySqlCommand cmd = new MySqlCommand(sql, conn);
- MySqlDataReader rdr = cmd.ExecuteReader();
- while (rdr.Read())
- {
- Console.WriteLine(rdr[0]+" -- "+rdr[1]);
- }
- rdr.Close();
- }
- catch (Exception ex)
- {
- Console.WriteLine(ex.ToString());
- }
- conn.Close(); // close the connection
- Console.WriteLine("Done the job");
- }
+public class DBConnect
+{
+    public static void Main()
+    {
+        string connStr = "server=localhost;user=root;database=world;port=3306;password=L3tM31n";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            Console.WriteLine("Connecting to MySQL--- world database");
+            conn.Open();
+            // Perform database operations
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        conn.Close(); // close the connection
+        Console.WriteLine("Done the job");
+    }
 
+}
